0ddca8907c6457cba93719e2a43da2522187beb6
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2024-07-28 19:05:24 +0900

    Add: Get users

--- node-mysql-crud/src/index.ts
+++ node-mysql-crud/src/index.ts
@@ -1,7 +1,27 @@
-import { createServer } from "http";
+import { createServer, IncomingMessage, ServerResponse } from "http";
 import { createConnection } from "./database";
+import { getUsers } from "./users";
 
-const server = createServer();
+const handleRequest = async (req: IncomingMessage, res: ServerResponse) => {
+  const [_, resource] = req.url?.split("/") || [];
+
+  if (resource !== "users") {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ massage: "Not Found" }));
+    return;
+  }
+
+  switch (req.method) {
+    case "GET":
+      await getUsers(res);
+      break;
+    default:
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ massage: "Not Found" }));
+  }
+};
+
+const server = createServer(handleRequest);
 
 const PORT = process.env.PORT || 3000;
 
--- node-mysql-crud/src/users.ts
+++ node-mysql-crud/src/users.ts
@@ -1,12 +1,13 @@
+import { ServerResponse } from "http";
 import { createConnection } from "./database";
-import { User } from "./type";
 
-export const getUsers = async () => {
+export const getUsers = async (res: ServerResponse) => {
   try {
     const connection = await createConnection();
     const [rows] = await connection.execute("select * from users");
     await connection.end();
-    return rows;
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(rows));
   } catch (error) {
     console.error("getUsers failed: ", JSON.stringify(error));
   }
