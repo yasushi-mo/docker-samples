c028642599728134d0a03bfa5b6734747dfbaa68
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2024-07-29 09:49:55 +0900

    Add: Error handling

--- node-mysql-crud/src/users.ts
+++ node-mysql-crud/src/users.ts
@@ -1,6 +1,12 @@
 import { ServerResponse } from "http";
 import { createConnection } from "./database";
 
+const handleError = (res: ServerResponse, error: unknown) => {
+  console.error("Error: ", error);
+  res.writeHead(500, { "Content-Type": "application/json" });
+  res.end(JSON.stringify({ message: "Internal Server Error" }));
+};
+
 export const getUsers = async (res: ServerResponse) => {
   try {
     const connection = await createConnection();
@@ -9,6 +15,6 @@ export const getUsers = async (res: ServerResponse) => {
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(JSON.stringify(rows));
   } catch (error) {
-    console.error("getUsers failed: ", JSON.stringify(error));
+    handleError(res, error);
   }
 };
