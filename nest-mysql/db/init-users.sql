eb7de783468ce06a6672edf6f27d57b4f86e95a0
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2024-08-06 10:05:53 +0900

    Add: Create Initial data file

---
+++ nest-mysql/db/init-users.sql
@@ -0,0 +1,3 @@
+CREATE TABLE users(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(32) NOT NULL, email VARCHAR(32) NOT NULL);
+INSERT users (name, email) VALUES ("Wyatt", "wyatt@example.com");
+INSERT users (name, email) VALUES ("Billy", "billy@example.com");
