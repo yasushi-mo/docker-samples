f5bcbba71914241f3a1d64bf8ec8955d56c93629
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2024-08-07 10:03:43 +0900

    Fix: DB host value

--- nest-mysql/src/ormconfig.ts
+++ nest-mysql/src/ormconfig.ts
@@ -2,7 +2,7 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 const config: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: 'localhost',
+  host: 'db',
   port: 3306,
   username: 'user',
   password: 'userpassword',
