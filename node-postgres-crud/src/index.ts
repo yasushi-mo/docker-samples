e2d91d4f2cc4b83821b2c2c6e8d2ef022942ee34
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2025-08-03 15:31:29 +0900

    Add: Call createConnection

--- node-postgres-crud/src/index.ts
+++ node-postgres-crud/src/index.ts
@@ -1,4 +1,5 @@
 import { createServer } from "http";
+import { createConnection } from "./database";
 
 const server = createServer();
 
@@ -6,4 +7,5 @@ const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  createConnection();
 });
