18d88c7324e0535b6ff6574e4e623f6a7741e60e
yasushi-mo <ymorita124@gmail.com>
2026-06-20 14:25:42 +0900

    findAllUsers

---
+++ spring-postgres-crud/src/main/java/com/example/spring_postgres_crud/UserController.java
@@ -0,0 +1,19 @@
+package com.example.spring_postgres_crud;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
