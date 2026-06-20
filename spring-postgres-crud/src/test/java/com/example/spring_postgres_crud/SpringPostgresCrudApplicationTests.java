2ff43c246f93669fea7ab7ee7b0be18fc3182082
yasushi-mo <ymorita124@gmail.com>
2026-06-20 11:59:16 +0900

    init spring-postgres-crud

---
+++ spring-postgres-crud/src/test/java/com/example/spring_postgres_crud/SpringPostgresCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_postgres_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPostgresCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
